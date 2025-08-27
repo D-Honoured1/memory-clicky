01ce357ba67d42fe40f17558982dffe1067e516f
D-Honoured1 <jamesalvez347@gmail.com>
2025-08-27 17:30:56 +0100

    Added ImageCard & minor fixes

--- src/hooks/useFetchImages.js
+++ src/hooks/useFetchImages.js
@@ -8,7 +8,9 @@ export default function useFetchImages(query = "nature") {
     async function fetchImages() {
       try {
         const response = await fetch(
-          `https://api.unsplash.com/search/photos?query=${query}&per_page=12&client_id=${import.meta.env.VITE_UNSPLASH_KEY}`
+          `https://api.unsplash.com/search/photos?query=${query}&per_page=12&client_id=${
+            import.meta.env.VITE_UNSPLASH_ACCESS_KEY
+          }`
         );
 
         const data = await response.json();
